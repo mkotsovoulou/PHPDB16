3ffbf1de93f00bfa0f2b4003b87238d32c5efa19
MairaKotsovoulou <mkotsovoulou@acg.edu>
2015-11-11 09:47:16 +0200

    v1

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+define("BASE_URL","/");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
+define("DB_HOST","localhost");
+define("DB_NAME","fall2015_demo");
+define("DB_PORT","8889");
+define("DB_USER","fall2015_root");
+define("DB_PASS","!root123#");
