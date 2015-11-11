3dfa425a68a91f51312ece92970717472645d42b
mairako <mkotsovoulou@acg.edu>
2015-11-11 11:45:01 +0200

    v3

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 define("BASE_URL","/");
 define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
-define("DB_HOST","localhost");
+define("DB_HOST","fall2015.students.acg.edu");
 define("DB_NAME","fall2015_demo");
 define("DB_PORT","8889");
 define("DB_USER","fall2015_root");
